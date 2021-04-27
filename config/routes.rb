fb77dd3c55fae894c0c4eeb176addbcedc13019c
kai-hosoi <kai228322@gmail.com>
2021-04-27 05:22:01 +0000

    rails 05 課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  
   root 'pages#index'
   get 'pages/help'
+  get 'pages/link'
   
   resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
