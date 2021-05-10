c20d90a95610b869b400e390ed83c3c6d23c2caa
kai-hosoi <kai228322@gmail.com>
2021-05-10 10:37:57 +0000

    0510課題途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   validates :name ,presence:true,length:{maximum:15}
   validates :email,presence:true,format:{with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
-  validates :password,length:{minimum:8,maximum:32},format:{with:VALID_PASSWORD_REGEX}
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{8,32}\z/
+  validates :password,format:{with:VALID_PASSWORD_REGEX}
   
   has_secure_password
   
   has_many :topics
   has_many :favorites
   has_many :favorite_topics, through: :favorites, source: 'topic'
+  has_many :comments
 end
